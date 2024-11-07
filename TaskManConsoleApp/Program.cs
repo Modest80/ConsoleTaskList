7e9920856efe8952f826157f9f3c0fcceea1833b
modest80 <modesting@gmail.com>
2024-11-07 21:29:21 +0400

    Основные файлы

---
+++ TaskManConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+namespace TaskManConsoleApp {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Title = "Список процессов";
+            Console.WindowWidth = 40;
+            Console.BufferWidth = 40;
+            Process[] prcs = Process.GetProcesses();
+            Console.WriteLine("{0,-28}{1,-10}", "Имя процесса", "PID");
+            foreach (Process p in prcs) {
+                Console.WriteLine("{0,-28}{1,-10}", p.ProcessName, p.Id);
+            }            
+        }
+    }
+}
